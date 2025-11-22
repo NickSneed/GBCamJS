34acc9b7b61c23098edaaad442635a19e753a59d
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2025-11-22 15:16:56 -0600

    optimizations

--- src/utils/createmontage.js
+++ src/utils/createmontage.js
@@ -1,41 +1,44 @@
 const WIDTH = 128;
 const HEIGHT = 112;
+const TOTAL_PIXELS = WIDTH * HEIGHT;
 
 const createHorizontalMontage = (photoData1, photoData2) => {
-    const montageData = [];
-    const halfHeightInPixels = (WIDTH * HEIGHT) / 2;
-    montageData.push(...photoData1.slice(0, halfHeightInPixels));
-    montageData.push(...photoData2.slice(halfHeightInPixels));
-    return montageData;
-};
-
-const createVerticalMontage = (photoData1, photoData2) => {
-    const montageData = [];
+    const montageData = new Uint8Array(TOTAL_PIXELS);
     const halfWidth = WIDTH / 2;
     for (let y = 0; y < HEIGHT; y++) {
         const rowStartIndex = y * WIDTH;
-        montageData.push(...photoData1.slice(rowStartIndex, rowStartIndex + halfWidth));
-        montageData.push(...photoData2.slice(rowStartIndex + halfWidth, rowStartIndex + WIDTH));
+        const rowEndIndex = rowStartIndex + WIDTH;
+        montageData.set(photoData1.subarray(rowStartIndex, rowStartIndex + halfWidth), rowStartIndex);
+        montageData.set(photoData2.subarray(rowStartIndex + halfWidth, rowEndIndex), rowStartIndex + halfWidth);
     }
     return montageData;
 };
 
+const createVerticalMontage = (photoData1, photoData2) => {
+    const montageData = new Uint8Array(TOTAL_PIXELS);
+    const halfHeightInPixels = TOTAL_PIXELS / 2;
+    montageData.set(photoData1.subarray(0, halfHeightInPixels));
+    montageData.set(photoData2.subarray(halfHeightInPixels), halfHeightInPixels);
+    return montageData;
+};
+
 const createQuadrantMontage = (photoData1, photoData2) => {
-    const montageData = [];
+    const montageData = new Uint8Array(TOTAL_PIXELS);
     const halfWidth = WIDTH / 2;
     const halfHeight = HEIGHT / 2;
 
     for (let y = 0; y < HEIGHT; y++) {
         const rowStartIndex = y * WIDTH;
+        const rowEndIndex = rowStartIndex + WIDTH;
 
         if (y < halfHeight) {
             // Top-left from photo1, top-right from photo2
-            montageData.push(...photoData1.slice(rowStartIndex, rowStartIndex + halfWidth));
-            montageData.push(...photoData2.slice(rowStartIndex + halfWidth, rowStartIndex + WIDTH));
+            montageData.set(photoData1.subarray(rowStartIndex, rowStartIndex + halfWidth), rowStartIndex);
+            montageData.set(photoData2.subarray(rowStartIndex + halfWidth, rowEndIndex), rowStartIndex + halfWidth);
         } else {
             // Bottom-left from photo2, bottom-right from photo1
-            montageData.push(...photoData2.slice(rowStartIndex, rowStartIndex + halfWidth));
-            montageData.push(...photoData1.slice(rowStartIndex + halfWidth, rowStartIndex + WIDTH));
+            montageData.set(photoData2.subarray(rowStartIndex, rowStartIndex + halfWidth), rowStartIndex);
+            montageData.set(photoData1.subarray(rowStartIndex + halfWidth, rowEndIndex), rowStartIndex + halfWidth);
         }
     }
     return montageData;
@@ -43,58 +46,58 @@ const createQuadrantMontage = (photoData1, photoData2) => {
 
 const createFourPhotoQuadrantMontage = (photos) => {
     const [photoData1, photoData2, photoData3, photoData4] = photos;
-    const montageData = [];
+    const montageData = new Uint8Array(TOTAL_PIXELS);
     const halfWidth = WIDTH / 2;
     const halfHeight = HEIGHT / 2;
 
     for (let y = 0; y < HEIGHT; y++) {
         const rowStartIndex = y * WIDTH;
+        const rowEndIndex = rowStartIndex + WIDTH;
 
         if (y < halfHeight) {
             // Top-left from photo1, top-right from photo2
-            montageData.push(...photoData1.slice(rowStartIndex, rowStartIndex + halfWidth));
-            montageData.push(...photoData2.slice(rowStartIndex + halfWidth, rowStartIndex + WIDTH));
+            montageData.set(photoData1.subarray(rowStartIndex, rowStartIndex + halfWidth), rowStartIndex);
+            montageData.set(photoData2.subarray(rowStartIndex + halfWidth, rowEndIndex), rowStartIndex + halfWidth);
         } else {
             // Bottom-left from photo3, bottom-right from photo4
-            montageData.push(...photoData3.slice(rowStartIndex, rowStartIndex + halfWidth));
-            montageData.push(...photoData4.slice(rowStartIndex + halfWidth, rowStartIndex + WIDTH));
+            montageData.set(photoData3.subarray(rowStartIndex, rowStartIndex + halfWidth), rowStartIndex);
+            montageData.set(photoData4.subarray(rowStartIndex + halfWidth, rowEndIndex), rowStartIndex + halfWidth);
         }
     }
     return montageData;
 };
 
 const createHorizontalTwoThirdsMontage = (photoData1, photoData2) => {
-    const montageData = [];
+    const montageData = new Uint8Array(TOTAL_PIXELS);
     const topHeight = 80;
     const splitPointInPixels = topHeight * WIDTH;
 
-    montageData.push(...photoData1.slice(0, splitPointInPixels));
-    montageData.push(...photoData2.slice(splitPointInPixels));
+    montageData.set(photoData1.subarray(0, splitPointInPixels));
+    montageData.set(photoData2.subarray(splitPointInPixels), splitPointInPixels);
     return montageData;
 };
 
 const createHorizontalBarsMontage = (photos) => {
     const [photoData1, photoData2, photoData3] = photos;
-    const montageData = [];
+    const montageData = new Uint8Array(TOTAL_PIXELS);
 
     const topHeight = 32;
     const middleHeight = 48;
 
     const topPixels = topHeight * WIDTH;
-    const middleStartRow = (HEIGHT - middleHeight) / 2; // 32
-    const middleStartPixel = middleStartRow * WIDTH;
-    const middleEndPixel = (middleStartRow + middleHeight) * WIDTH; // 80 * 128
-    const bottomStartPixel = (HEIGHT - topHeight) * WIDTH; // 80 * 128
+    const middleStartPixel = 32 * WIDTH;
+    const middleEndPixel = (32 + middleHeight) * WIDTH;
+    const bottomStartPixel = (HEIGHT - topHeight) * WIDTH;
 
-    montageData.push(...photoData1.slice(0, topPixels));
-    montageData.push(...photoData2.slice(middleStartPixel, middleEndPixel));
-    montageData.push(...photoData3.slice(bottomStartPixel));
+    montageData.set(photoData1.subarray(0, topPixels));
+    montageData.set(photoData2.subarray(middleStartPixel, middleEndPixel), topPixels);
+    montageData.set(photoData3.subarray(bottomStartPixel), topPixels + (middleEndPixel - middleStartPixel));
 
     return montageData;
 };
 
 const createBorderMontage = (photoData1, photoData2) => {
-    const montageData = [...photoData1]; // Start with the border image
+    const montageData = new Uint8Array(photoData1); // Start with the border image
     const innerWidth = 80;
     const innerHeight = 64;
 
@@ -102,24 +105,11 @@ const createBorderMontage = (photoData1, photoData2) => {
     const offsetX = (WIDTH - innerWidth) / 2; // (128 - 80) / 2 = 24
     const offsetY = (HEIGHT - innerHeight) / 2; // (112 - 64) / 2 = 24
 
-    // Calculate source offsets to take the center of the second photo
-    const sourceOffsetX = (WIDTH - innerWidth) / 2;
-    const sourceOffsetY = (HEIGHT - innerHeight) / 2;
-
     for (let y = 0; y < innerHeight; y++) {
-        for (let x = 0; x < innerWidth; x++) {
-            // Destination coordinates in the final montage
-            const destX = offsetX + x;
-            const destY = offsetY + y;
-            const destIndex = destY * WIDTH + destX;
-
-            // Source coordinates from the second photo
-            const sourceX = sourceOffsetX + x;
-            const sourceY = sourceOffsetY + y;
-            const sourceIndex = sourceY * WIDTH + sourceX;
-
-            montageData[destIndex] = photoData2[sourceIndex];
-        }
+        const sourceRowStartIndex = (offsetY + y) * WIDTH + offsetX;
+        const sourceRowEndIndex = sourceRowStartIndex + innerWidth;
+        const destRowStartIndex = (offsetY + y) * WIDTH + offsetX;
+        montageData.set(photoData2.subarray(sourceRowStartIndex, sourceRowEndIndex), destRowStartIndex);
     }
     return montageData;
 };
@@ -150,11 +140,11 @@ const createMontage = (photos, splitType = 'horizontal') => {
                 return [];
             }
             return createHorizontalBarsMontage(photos);
+        case 'horizontal':
         case 'vertical':
         case 'quadrant':
         case 'border':
         case 'horizontal-2/3':
-        case 'horizontal':
         default: {
             if (photos.length < 2 || !photos[0] || !photos[1]) {
                 return [];
@@ -166,7 +156,7 @@ const createMontage = (photos, splitType = 'horizontal') => {
             if (splitType === 'horizontal-2/3') {
                 return createHorizontalTwoThirdsMontage(photoData1, photoData2);
             }
-            return createHorizontalMontage(photoData1, photoData2);
+            return createHorizontalMontage(photoData1, photoData2); // Default to horizontal
         }
     }
 };
