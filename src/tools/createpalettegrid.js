a6386b533a35bfcd952c301531f9ec112a537c2e
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2026-07-29 19:53:47 -0500

    updated svg

--- src/tools/createpalettegrid.js
+++ src/tools/createpalettegrid.js
@@ -48,7 +48,8 @@ export const generatePaletteGridSvg = (paletteEntries, options = {}) => {
         })
         .join('');
 
-    return `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" style="background-color: transparent;">
+  <rect width="100%" height="100%" fill="none" />
   ${svgBody}
 </svg>`;
 };
